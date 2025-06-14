cc588c7469ce2149a5e744c1333a29e78d8fe12f
Devtimmy <oyinloyematthew6@gmail.com>
2025-06-14 01:07:50 +0100

    Done with Thank you page

--- src/Thanks.jsx
+++ src/Thanks.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import thank from '../assets/thanks.jpg'
+import thank from './assets/thanks.jpg'
+
+
 
 const Thanks = () => {
 
@@ -7,7 +9,16 @@ const Thanks = () => {
     <div>
       
       <div className="flex flex-col items-center justify-center gap-16 text-center">
-        <h2 className="pt-32">
+
+        <div className="w-60 h-60 text-orange-600 mx-auto mt-16">
+          <img className="rounded-md w-60 h-60 lg:w-60 lg:h-60 hover:scale-105 transform transition duration-500" src={thank} />
+        </div>
+
+        <h2 className="text-4xl">
+          Thank You!!!
+        </h2> 
+        
+        <h2>
           Thank you so much for joining DevTimmy’s Virtual Birthday Party. Really glad to have you made it here today
         </h2>
 
@@ -21,13 +32,7 @@ const Thanks = () => {
 
         <a className="bg-[#27548A] py-4 px-8 rounded-md shadow-div-shadow transform hover:scale-105 hover:bg-[#332D56] transition duration-300" href="https://forms.gle/Who85YYfZJTpY9n86">Click Here</a>
 
-        <h2 className="text-4xl">
-          Thank You!!!
-        </h2> 
-        <div className="w-32 h-32 text-orange-600 mx-auto ">
-          <img className="rounded-md w-32 h-32 lg:w-32 lg:h-32 hover:scale-105 transform transition duration-500" src={ thank } />
-        </div>
-        
+     
       </div>
     </div>
   )
